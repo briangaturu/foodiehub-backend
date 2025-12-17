bac09376b00273e7f9a938b804df7a397f814c41
briangaturu <briangaturu03@gmail.com>
2025-12-17 18:05:58 +0300

    produbuild

--- src/users/user.route.ts
+++ src/users/user.route.ts
@@ -12,13 +12,13 @@ userRouter.get('/users', adminRoleAuth, getUsers);
  
 // Get user by ID
 userRouter.get('/users/:id', getUserById);
- 
+
 // Create a new user
-userRouter.post('/users', createUser);
- 
+userRouter.post('/users', adminRoleAuth, createUser);
+
 // Update an existing user
-userRouter.put('/users/:id',updateUser);
- 
+userRouter.put('/users/:id', adminRoleAuth, updateUser);
+
  
 // Delete an existing user
 userRouter.delete('/users/:id', deleteUser);
